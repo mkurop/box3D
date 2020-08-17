fe42f6d1fc9bc18e48253c687426c9bfe5d2bb97
Robert Kolke <robertoskarkolke@gmail.com>
2020-08-17 14:15:47 +0200

    Pierwsza wersja programu + notatka

--- PRAKTYKI - Pudełka.py
+++ PRAKTYKI - Pudełka.py
@@ -9,32 +9,44 @@ try:
     os.remove('3d_index.dat')
 except:
     pass
+
+
 class box3D:
     def __init__(self, interval_x, interval_y, interval_z):
         self.interval_x = interval_x
         self.interval_y = interval_y
         self.interval_z = interval_z
+
     def get_interval_x(self):
         return self.interval_x
+
     def get_interval_y(self):
         return self.interval_y
+
     def get_interval_z(self):
         return self.interval_z
 
     @staticmethod
     def factory(x1, y1, z1, x2, y2, z2):
         return box3D(closed(x1, x2), closed(y1, y2), closed(z1, z2))
+
+
 class boxStack:
     def __init__(self):
         self.stack = []
+
     def get_stack(self):
         return self.stack
+
     def set_stack(self, new_stack):
         self.stack = new_stack
+
     def append(self, added):
         self.stack.append(added)
+
     def pop(self):
         return self.stack.pop()
+
 
 class tree:
     def __init__(self):
@@ -43,17 +55,35 @@ class tree:
         # ustawienia drzewa
         self.properties.dimension = 3
         self.tree = rtree.index.Index('3d_index', properties=self.properties)
+
     def get_tree(self):
         return self.tree
+
     def set_tree(self, new_tree):
         self.tree = new_tree
 
+
 class algorytm:
+    def permute(sortin, permutation):
+        assert len(sortin) == len(permutation)
+        return [sortin[i] for i in permutation]
+
+    def my_sort(inputlist):
+        inputlist = zip(inputlist, range(len(inputlist)))
+        aux = sorted(inputlist, key = lambda x:x[0])
+        sorted2in = [aux[i][1] for i in range(len(aux))]
+        list2 = zip(sorted2in, range(len(sorted2in)))
+        aux1 = sorted(list2, key = lambda x:x[0])
+        in2sorted = [aux1[i][1] for i in range(len(aux1))]
+        sort = [aux[i][0] for i in range(len(aux))]
+        return sort, in2sorted, sorted2in
+
     def divide_in(self, int1, int2):
         return int1 | int2
+
     def divide_half_out(self, int1, int2):
         up, low = (int1.upper, int2.upper), (int1.lower, int2.lower)
-        if int2.lower==int1.lower:
+        if int2.lower == int1.lower:
             return closed(min(low), min(up)), openclosed(min(up), max(up))
         else:
             return closed(min(low), max(low)), openclosed(max(low)+1, max(up))
@@ -63,14 +93,19 @@ class algorytm:
 
     def is_in(self, interval1, interval2):
         return True if interval1 & interval2 == min(interval2, interval1) else False
+
     def is_equal(self, interval1, interval2):
-        return True if interval1==interval2 else False
+        return True if interval1 == interval2 else False
+
     def is_out(self, interval1, interval2):
-        return True if (interval1 & interval2)!=(interval1|interval2) and any(interval1&interval2)  else False
+        return True if (interval1 & interval2) != (interval1|interval2) and any(interval1&interval2)  else False
+
     def is_separate(self, int1, int2):
         return False if any(int1 & int2) else True
+
     def is_half_out(self, int1, int2):
-        return True if (int1.lower == int2.lower or int2.upper == int1.upper) and int1|int2!=int1&int2 else False
+        return True if (int1.lower == int2.lower or int2.upper == int1.upper) and int1 | int2 != int1 & int2 else False
+
     def rozbijanie(self, q, i):
         if self.is_in(q, i):
             return self.divide_in(q,i)
@@ -82,9 +117,10 @@ class algorytm:
             return self.divide_equal(q,i)
         elif self.is_separate(q, i):
             return q
+
     def rozbij(self, q, i):
         x, y, z = self.rozbijanie(q.interval_x, i.interval_x), self.rozbijanie(q.interval_y, i.interval_y), self.rozbijanie(q.interval_z, i.interval_z)
-        print(list(it.combinations_with_replacement([x,y,z],3)))
+        print(list(it.combinations_with_replacement([x, y, z], 3)))
 
     @staticmethod
     # początek funkcji głównej
@@ -95,11 +131,11 @@ class algorytm:
             # komenda pop
             q = Q.pop()
             # sprawdzanie czy w drzewie pudełko q się przecina
-            if list(tree.tree.intersection([q.interval_x.lower,q.interval_y.lower,q.interval_z.lower, q.interval_x.upper,q.interval_y.upper,q.interval_z.upper])):
+            if list(tree.tree.intersection([q.interval_x.lower, q.interval_y.lower, q.interval_z.lower, q.interval_x.upper, q.interval_y.upper, q.interval_z.upper])):
                 # zwrócenie z drzewa pierwszego obiektu, z którym pudełko się przecina
                 i = list(tree.tree.intersection((q.interval_x.lower, q.interval_y.lower,q.interval_z.lower, q.interval_x.upper, q.interval_y.upper, q.interval_z.upper), True))
                 inter = [item for item in i[0].bounds]
-                i = box3D.factory(inter[0],inter[1],inter[2],inter[3],inter[4],inter[5])
+                i = box3D.factory(inter[0], inter[1], inter[2], inter[3], inter[4], inter[5])
                 # dodanie na koniec zbioru Q "rozbitego" pudełka q
                 divided = [i for i in algorytm().rozbij(q, i)]
                 print(divided)
@@ -114,18 +150,19 @@ class algorytm:
         lista = list(tree.tree.intersection(tree.tree.get_bounds(),True))
         lista = [(item.bbox)for item in lista]
         for i in lista:
-            print([i[0],i[3]],'x', [i[1], i[4]],'x',[i[2],i[5]],'\n')
-        # zwrócenie drzewa
+            print([i[0], i[3]], 'x', [i[1], i[4]], 'x', [i[2],i[5]], '\n')
+        #zwrócenie drzewa
         return tree.tree
 
-###pudelka = int(input('\nIle pudełek?\n'))
+#pudelka = int(input('\nIle pudełek?\n'))
+
+
 Q = boxStack()
 # pętla do wczytywania współrzędnych dla każdego pudełka i
 pudelka = int(input('Ile pudełek?: '))
 for i in range(pudelka):
     # (int(num) for num in input('\nPodaj 6 liczb dla jednego z pudełek oddzielonput()), int(input()), int(input()), ine spacją oraz przecinkiem: '))
-    Q.append(box3D.factory(int(input("Podaj po jednej z sześciu współrzędnych pudełka oddzielonych enterem:\n")), \
-                           int(input()), int(input()), int(input()), int(input()), int(input())))
+    Q.append(box3D.factory(int(input("Podaj po jednej z sześciu współrzędnych pudełka oddzielonych enterem:\n")), int(input()), int(input()), int(input()), int(input()), int(input())))
 algorytm.algorytm(Q, tree())
 
 
