8ebd15927bf02b4e75f632477e693fa1b339178d
KolRobOsk <69192186+KolRobOsk@users.noreply.github.com>
2020-11-01 16:51:51 +0100

    Add files via upload

--- docs/example.py
+++ docs/example.py
@@ -2,9 +2,10 @@ Q = boxStack()
 table = []
 pudelka = int(input())
 #deklaracja stosu, tablicy pomocnej i liczby pudełek
-for i in range(pudelka):
-    table.append(box3D.factory(int(input()), int(input()), int(input()), int(input()), int(input()), int(input())))
-#pętla tworząca pudełka na podstawie podanych danych
+table.append(box3D.factory(2, 1, 4, 5, 7, 8))
+table.append(box3D.factory(1, 2, 3, 3, 5, 7))
+table.append(box3D.factory(2, 3, 2, 4, 7, 7))
+#tworzenie pudełek (funkcja factory ma kolejność współrzędnych x_lower y_lower z_lower x_upper, y_upper, z_upper)
 #w kolejności x_lower, y_lower, z_lower, x_upper, y_upper, z_upper
 Q.extend(table)
 #dodanie pudełek do stosu
