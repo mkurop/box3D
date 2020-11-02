dd08673fd149d08cb0ae33c8b558c971e8aa0f09
KolRobOsk <69192186+KolRobOsk@users.noreply.github.com>
2020-11-02 10:57:32 +0100

    2.11 11:00

--- docs/example.py
+++ docs/example.py
@@ -1,21 +1,19 @@
-import os, sys
-sys.path.insert(0, os.path.abspath('..'))
+#przykładowy program należy wywołać wewnątrz katalogu ./Praktyki
 from mainalgo import *
 #import głównego modułu
-Q = boxStack()
 table = []
-#deklaracja stosu, tablicy pomocnej i liczby pudełek
+#deklaracja tablicy pomocnej
 table.append(box3D.factory(2, 1, 4, 5, 7, 8))
 table.append(box3D.factory(1, 2, 3, 3, 5, 7))
 table.append(box3D.factory(2, 3, 2, 4, 7, 7))
 #tworzenie pudełek (funkcja factory ma kolejność współrzędnych x_lower y_lower z_lower x_upper, y_upper, z_upper)
 #w kolejności x_lower, y_lower, z_lower, x_upper, y_upper, z_upper
-for i in table:
-    i.__str__()
+for box in table:
+    print(box)
     print('\n')
 #wypisywanie pudełek przed wstawieniem ich do drzewa
 pudelka = algorithm.execute(table)
 #uruchomienie funkcji execute i rozbicie pudełek
-for i in pudelka:
-    print(i.__str__())
+for box in pudelka:
+    print(box)
     #wypisanie pudełek końcowych
